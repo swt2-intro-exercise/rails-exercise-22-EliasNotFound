8e090a75f0cdf9fb96d44282ae5bcf430104bc60
Elias <elias.fiedler@gmx.de>
2022-11-09 13:58:00 +0100

    add test for issue #4

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -12,4 +12,12 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
+  it "should create a new author in the database" do
+    visit new_author_path
+    page.fill_in 'author[first_name]', with: 'Ada'
+    page.fill_in 'author[last_name]', with: 'Lovelace'
+    page.fill_in 'author[homepage]', with: 'https://de.wikipedia.org/wiki/Ada_Lovelace'
+    find('input[type="submit"]').click
+  end
 end
